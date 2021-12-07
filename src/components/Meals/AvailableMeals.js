8b34847d34036b3b37a95520e51b4da8b13b3ecb
Tiago Lopes <36971635+tiagu2s@users.noreply.github.com>
2021-12-07 15:34:21 +0000

    get meals data from backend, handling loading state and handle errors

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,63 @@
-import Card from '../UI/Card';
-import MealItem from './MealItem/MealItem';
-import classes from './AvailableMeals.module.css';
+import { useEffect, useState } from "react";
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [totalMeals, setTotalMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    const getMeals = async () => {
+      const response = await fetch(
+        "https://order-food-app-6dbf9-default-rtdb.firebaseio.com/meals.json"
+      );
+
+      if (!response.ok) {
+        return new Error("Something went wrong");
+      }
+
+      const data = await response.json();
+      const loadedMeals = [];
+
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      }
+
+      setTotalMeals(loadedMeals);
+      setIsLoading(false);
+    };
+
+    getMeals().catch((error) => {
+      setIsLoading(false);
+      setError(error.message);
+    });
+  }, []);
+
+  if (isLoading && !error) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading..</p>
+      </section>
+    );
+  }
+
+  if (error && !isLoading) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{error}</p>
+      </section>
+    );
+  }
+
+  const mealsList = totalMeals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
